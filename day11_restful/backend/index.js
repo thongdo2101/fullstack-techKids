4d34eda24846427e08eb892bc44ab31fb8fdffdf
Thong Do <dovanthong97@gmail.com>
2018-04-16 09:19:26 +0700

    update day11

--- day11_restful/backend/index.js
+++ day11_restful/backend/index.js
@@ -1,14 +1,29 @@
+// import a framework for Nodejs
 const express = require('express');
+
+// import a library of nodejs for connecting with mongodb 
 const mongoose = require('mongoose');
+
+// import a middleware(lớp trung gian) manipulate with JSON, text and encrypt URL
 const bodyParser = require('body-parser');
+
+// import images router to app
 const imageRouter = require('./modules/api/images/router');
+
+// initialize the app
 const app = express();
 
+// return req.body contains key-value pairs, 
+// where the value can be a string or array(when extended is false) 
+// or any type (when extended is true)
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+
+// register a router
 app.use('/api/images', imageRouter);
 
+// Connect with DB
 mongoose.connect('mongodb://localhost:27017/tk-hotgirls', (err) => {
     if (err) {
         console.error(err);
@@ -17,7 +32,10 @@ mongoose.connect('mongodb://localhost:27017/tk-hotgirls', (err) => {
     }
 });
 
+// create a port
 const port = process.env.port || 6969;
+
+// Start server
 app.listen(port, (err) => {
     if (err) console.log(err);
     console.log("Server started at port: " + port);
