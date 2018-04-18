b06b746673518e9ed2265d13d0b909f46073d206
Thong Do <dovanthong97@gmail.com>
2018-04-18 20:09:48 +0700

    update regex

--- day11_restful/backend/modules/api/users/model.js
+++ day11_restful/backend/modules/api/users/model.js
@@ -13,7 +13,14 @@ const userSchema = new Schema({
     Email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        validate: {
+            validator: function (value) {
+                const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                return regex.test(value);
+            },
+            message: "{VALUE} is not a valid email address"
+        }
     },
     Username: {
         type: String,
